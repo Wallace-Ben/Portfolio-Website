0433739aac278b2c6023083b48701b184c69ff19
Wallace-Ben <125564099+Wallace-Ben@users.noreply.github.com>
2026-01-12 14:38:35 +0000

    More screen size stylings

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,27 +4,31 @@ import profilePhoto from "../../assets/images/linkedin-photo.jpg";
 
 export default function About() {
   return (
-    <div className="flex flex-col p-5 max-w-6xl mx-auto md:items-start items-center md:px-0">
-      <h1 className="xl:text-4xl text-2xl font-semibold text-emerald-500 md:mb-0 mb-2">
+    <div className="flex flex-col p-5 md:px-8 max-w-6xl mx-auto xl:items-start items-center xl:px-0">
+      <h1 className="xl:text-4xl text-2xl font-semibold text-emerald-500 xl:mb-0 mb-2">
         About Me
       </h1>
+
       <div
-        className="md:hidden shrink-0  h-[150px] w-[150px] md:translate-x-0 md:h-[170px] md:w-[170px] rounded-[20%] 
-        bg-cover bg-center bg-no-repeat border-3 border-emerald-500 md:shadow-md shadow-sm shadow-emerald-deep md:mb-0 mb-4
+        className="xl:hidden shrink-0 h-[150px] w-[150px] md:h-40 md:w-40 xl:translate-x-0 xl:h-[170px] xl:w-[170px] rounded-[20%] 
+        bg-cover bg-center bg-no-repeat border-3 border-emerald-500 xl:shadow-md shadow-sm shadow-emerald-deep xl:mb-0 mb-4
+        md:mb-6
     "
         style={{
           backgroundImage: `url(${profilePhoto.src})`,
           backgroundSize: 300,
         }}
       />
-      <p className="text-emerald-500 text-lg text-center">
+
+      <p className="text-emerald-500 text-lg text-center md:max-w-3xl">
         Building accessible, user-friendly interfaces with TypeScript and React.
       </p>
 
       <div className="max-w-5xl">
-        <div className="mt-5 md:mt-2 md:mb-5 mb-8 md:text-start text-center md:px-0 px-8 ">
+        <div className="mt-5 xl:mt-2 xl:mb-5 mb-8 xl:text-start text-center xl:px-0 px-8 md:px-4">
           <h2 className="font-semibold text-xl text-emerald-500">Summary</h2>
-          <div className="flex flex-row gap-8 items-start">
+
+          <div className="flex flex-row gap-8 items-start md:gap-6">
             <p className="text-charcoal flex-1">
               I&apos;m a frontend developer at Version 1, working closely with
               designers and backend engineers to deliver high-quality,
@@ -32,9 +36,10 @@ export default function About() {
               worked in data analysis, specialising in UX/UI analytics, which
               has shaped how I approach designing and building systems.
             </p>
+
             <div
-              className="hidden md:block shrink-0 -mt-25 h-[100px] w-[100px] md:translate-x-0 md:h-[170px] md:w-[170px] rounded-[20%] 
-        bg-cover bg-center bg-no-repeat border-3 border-emerald-500 md:shadow-md shadow-sm shadow-emerald-deep md:mb-0 mb-4
+              className="hidden xl:block shrink-0 -mt-25 h-[100px] w-[100px] xl:translate-x-0 xl:h-[170px] xl:w-[170px] rounded-[20%] 
+        bg-cover bg-center bg-no-repeat border-3 border-emerald-500 xl:shadow-md shadow-sm shadow-emerald-deep xl:mb-0 mb-4
     "
               style={{
                 backgroundImage: `url(${profilePhoto.src})`,
@@ -43,12 +48,14 @@ export default function About() {
             />
           </div>
         </div>
-        <div className="md:text-start text-center md:px-0 px-8">
+
+        <div className="xl:text-start text-center xl:px-0 px-8 md:px-4">
           <h2 className="font-semibold text-xl text-emerald-500">
             Experience Highlights
           </h2>
-          <div className="mb-8 md:mb-5">
-            <span className="flex md:flex-row flex-col justify-between">
+
+          <div className="mb-8 xl:mb-5">
+            <span className="flex xl:flex-row flex-col justify-between md:items-center">
               <h3 className="font-semibold text-emerald-500">
                 Frontend Developer - Version 1
               </h3>
@@ -64,8 +71,9 @@ export default function About() {
               guarantee reliability.
             </p>
           </div>
-          <div className="mb-8 md:mb-5">
-            <span className="flex md:flex-row flex-col justify-between">
+
+          <div className="mb-8 xl:mb-5">
+            <span className="flex xl:flex-row flex-col justify-between md:items-center">
               <h3 className="font-semibold text-emerald-500">
                 Data Analyst - Jarmany
               </h3>
@@ -82,17 +90,19 @@ export default function About() {
           </div>
         </div>
 
-        <div className="mb-10 text-center md:text-start md:px-0 px-8">
-          <h2 className="font-semibold text-xl text-emerald-500 md:mb-0 mb-1">
+        <div className="mb-10 text-center xl:text-start xl:px-0 px-8 md:px-4">
+          <h2 className="font-semibold text-xl text-emerald-500 xl:mb-0 mb-1">
             Core skills
           </h2>
-          <span className="flex md:flex-row flex-col">
+
+          <span className="flex xl:flex-row flex-col md:flex-row md:justify-center xl:justify-start">
             <h4 className="text-emerald-500 font-semibold mr-2">Frontend:</h4>
             <p className="text-charcoal">
               TypeScript, JavaScript, React, Next.js, HTML, CSS
             </p>
           </span>
-          <span className="flex flex-col md:flex-row">
+
+          <span className="flex flex-col xl:flex-row md:flex-row md:justify-center xl:justify-start">
             <h4 className="text-emerald-500 font-semibold mr-2">
               Data & backend:
             </h4>
@@ -100,12 +110,14 @@ export default function About() {
               MongoDB, MySQL, REST APIs, Python (basic)
             </p>
           </span>
-          <span className="flex flex-col md:flex-row">
+
+          <span className="flex flex-col xl:flex-row md:flex-row md:justify-center xl:justify-start">
             <h4 className="text-emerald-500 font-semibold mr-2">Tools: </h4>
             <p className="text-charcoal">Git, GitHub, Jest, Docker</p>
           </span>
         </div>
-        <div className="flex justify-center">
+
+        <div className="flex justify-center md:gap-4 mb-2">
           <LinkButton
             link="/projects"
             type={ButtonType.Primary}
